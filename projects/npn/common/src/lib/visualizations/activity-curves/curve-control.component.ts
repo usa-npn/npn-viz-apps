65444312e21eb9062ba94c3e812413e6bd60f125
Kevin Wong <kevin@usanpn.org>
2019-12-09 10:47:27 -0700

    Expanded date range for activity curves so that it includes the current year

--- projects/npn/common/src/lib/visualizations/activity-curves/curve-control.component.ts
+++ projects/npn/common/src/lib/visualizations/activity-curves/curve-control.component.ts
@@ -75,7 +75,7 @@ export class CurveControlComponent extends MonitorsDestroy {
         let thisYear = (new Date()).getFullYear(),
             years: number[] = [],
             c = 2010;
-        while(c < thisYear) {
+        while(c <= thisYear) {
             years.push(c++);
         }
         return years;

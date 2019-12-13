7905e9bf4d4b73978ad78c46906e18a5d2866143
Mark <mkimmet@gmail.com>
2019-12-12 23:00:20 -0500

    add fields to station

--- projects/npn/common/src/lib/common/station.ts
+++ projects/npn/common/src/lib/common/station.ts
@@ -5,6 +5,10 @@ export interface Station {
     network_id:number;
     station_id:number;
     station_name:string;
+    num_individuals?:number;
+    num_records?:number;
+    site_name?:string;
+    group_name?:string;
     // allow arbitrary keys for use by controls
     [x:string]:any;
 }

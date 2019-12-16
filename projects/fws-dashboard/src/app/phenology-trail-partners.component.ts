50a0ede081b173f42314f21097b543ba27e38c42
Mark <mkimmet@gmail.com>
2019-12-16 16:39:51 -0500

    map with legend pretty much done

--- projects/fws-dashboard/src/app/phenology-trail-partners.component.ts
+++ projects/fws-dashboard/src/app/phenology-trail-partners.component.ts
@@ -1,20 +1,20 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, HostListener } from '@angular/core';
 import { I18nPluralPipe } from '@angular/common';
 import {PhenologyTrail} from './entity.service';
-import {Network, Station, NetworkService, StationService, getStaticColor} from '@npn/common';
+import {Network, Station, NetworkService, StationService, getStaticColor, MAP_STYLES} from '@npn/common';
+import { MapsAPILoader, AgmMap, LatLngBounds, LatLngBoundsLiteral} from '@agm/core';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'phenology-trail-partners',
   template: `
-  <agm-map [latitude]="lat" [longitude]="lng">
+  <agm-map #AgmMap [streetViewControl]="false" [scrollwheel]="false" [styles]="mapStyles">
     <agm-marker
-    *ngFor="let station of stations | async; let i = index"
-    [latitude]="station.latitude"
-    [longitude]="station.longitude"
-    [opacity]="1"
-    [markerDraggable]="true"
-    (markerClick)="selectMarker(station)"
-    [iconUrl]="purpleMarker"
+        *ngFor="let station of stations | async; let i = index"
+        [latitude]="station.latitude"
+        [longitude]="station.longitude"
+        [iconUrl]="station.icon"
+        (markerClick)="selectMarker(station)"
     ></agm-marker>
     <agm-info-window *ngIf="griddedLoading"
                     [isOpen]="griddedOpen"
@@ -25,26 +25,35 @@ import {Network, Station, NetworkService, StationService, getStaticColor} from '
     <agm-info-window [isOpen]="!!selectedMarker" (infoWindowClose)="selectedMarker = null"
     [latitude]="selectedMarker?.latitude" [longitude]="selectedMarker?.longitude" *ngIf="selectedMarker">
         <div class="info-window-card">
-            <h1>{{selectedMarker.site_name}}</h1>
-            <h2>{{selectedMarker.group_name}}</h2>
+            <h3>{{selectedMarker.site_name}}</h3>
+            <h4>{{selectedMarker.group_name}}</h4>
             <ul>
-                <li><h1>{{selectedMarker.num_individuals}}</h1> {{selectedMarker.num_individuals | i18nPlural: itemPluralMapping['individual'] }}</li>
-                <li><h1>{{selectedMarker.num_records}}</h1> {{selectedMarker.num_records | i18nPlural: itemPluralMapping['record']}}</li>
+                <li><h3>{{selectedMarker.num_individuals}}</h3> {{selectedMarker.num_individuals | i18nPlural: itemPluralMapping['individual'] }}</li>
+                <li><h3>{{selectedMarker.num_records}}</h3> {{selectedMarker.num_records | i18nPlural: itemPluralMapping['record']}}</li>
             </ul>
         </div>
     </agm-info-window>
     
   </agm-map>
+  <div class="map-legend">
+    <h3>Legend</h3>
+    <ul>
+        <ng-container *ngFor="let network of networks | async; let i = index">
+            <li *ngIf="!!network"><div class="legend-box" [style.background-color]="network.colors?.color"></div> {{network.name}}</li>
+        </ng-container>
+    </ul>
+  </div>
   `,
   styleUrls: ['./phenology-trail-partners.component.scss']
 })
 export class PhenologyTrailPartnersComponent implements OnInit {
+    @ViewChild('AgmMap') agmMap: AgmMap;
     @Input() entity:PhenologyTrail;
     networks:Promise<Network[]>;
     stations:Promise<Station[]>;
-    lat = 35;
-    lng = -106.2615581;
     selectedMarker;
+    bounds:google.maps.LatLngBounds;
+    map: google.maps.Map;
     itemPluralMapping = {
         'record': {
           '=0' : 'Records',
@@ -62,31 +71,66 @@ export class PhenologyTrailPartnersComponent implements OnInit {
     griddedOpen:boolean = false;
     //griddedData:GriddedPointData;
     griddedLoading:boolean = false;
-    greenMarker = 'https://www.google.com/intl/en_us/mapfiles/ms/micons/green-dot.png';
-    yellowMarker = 'https://www.google.com/intl/en_us/mapfiles/ms/micons/yellow-dot.png';
-    purpleMarker = 'https://www.google.com/intl/en_us/mapfiles/ms/micons/purple-dot.png';
+    mapStyles: any[] = MAP_STYLES;
 
     constructor(
         private networkService:NetworkService,
-        private stationService:StationService
+        private stationService:StationService,
+        private mapsAPILoader:MapsAPILoader
         )  { }
 
     ngOnInit() {
         // map of network_id to a color
         const colorMap:any = this.entity.network_ids.reduce((map,id,i) => {
-            map[`${id}`] = getStaticColor(i);
+            const color = getStaticColor(i);
+            map[`${id}`] = { 
+                color: color,
+                outline: d3.color(color).darker().toString()
+            }
             return map;
         },{});
 
         const mapColors = list => list.map(o => {
-            o.color = colorMap[`${o.network_id}`];
+            o.colors = colorMap[`${o.network_id}`];
             return o;
         });
 
-        this.networks = this.networkService.getNetworks(this.entity.network_ids);
-        this.stations = this.networkService.getStations(this.entity.network_ids).then(mapColors);
+        this.networks = this.networkService.getNetworks(this.entity.network_ids).then(mapColors);
+        this.stations = this.networkService.getStations(this.entity.network_ids).then(mapColors).then(stations => {
+            return this.mapsAPILoader.load().then( () => {
+                stations.forEach(station => {
+                    station.icon = this.newGoogleMapsSymbol(station);
+                });
+                return stations;
+            });
+            
+        });
+
         console.log(this.networks);
         console.log(this.stations);
+    }
+
+    ngAfterViewInit() {
+        //console.log(this.agmMap);
+        //cycle through the map elements and get the center position of the map
+        this.agmMap.mapReady.subscribe(map => {
+          this.map = map;
+          this.stations.then(stations => {
+            const bounds = new google.maps.LatLngBounds();
+            for (const mm of stations) {
+                bounds.extend(new google.maps.LatLng(mm.latitude, mm.longitude));
+            }
+            this.bounds = bounds;
+            this.resize();
+          });
+        });
+      }
+
+    @HostListener('window:resize') 
+    resize(){
+        if(this.map && this.bounds){
+            this.map.fitBounds(this.bounds);
+        }
     }
 
     /**
@@ -97,11 +141,21 @@ export class PhenologyTrailPartnersComponent implements OnInit {
         this.griddedLoading = true;
         this.griddedLat = selectedStation.latitude;
         this.griddedLng = selectedStation.longitude;
-        //this.griddedData = null;
         this.griddedOpen = true;
         this.stationService.getStation(selectedStation.station_id).then(station =>{
             this.selectedMarker = station;
             this.griddedLoading = false;
         });
+    }
+
+    newGoogleMapsSymbol(station:Station):google.maps.Symbol {
+        return {
+            path: google.maps.SymbolPath.CIRCLE,
+            scale: 7,
+            fillOpacity: 1,
+            fillColor: station.colors.color,
+            strokeColor: station.colors.outline,
+            strokeWeight:1
+        };
     }
 }

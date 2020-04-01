5cdfa8ade2258efebaa6dec532394b9fc203905b
Jeff Switzer <jeff.switzer@gmail.com>
2020-04-01 15:23:44 -0700

    add safeHtml pipe

--- projects/fws-dashboard/src/app/resources.component.ts
+++ projects/fws-dashboard/src/app/resources.component.ts
@@ -1,10 +1,19 @@
-import {Component,Input} from '@angular/core';
+import {Component,Input, Pipe, PipeTransform} from '@angular/core';
 import {EntityBase} from './entity.service';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Pipe({ name: 'safeHtml'})
+export class SafeHtmlPipe implements PipeTransform  {
+  constructor(private sanitized: DomSanitizer) {}
+  transform(value) {
+    return this.sanitized.bypassSecurityTrustHtml(value);
+  }
+}
 
 @Component({
     selector: 'fws-dashboard-resources',
     template: `
-    <div [innerHtml]="entity && entity.resources ? entity.resources : ''"></div>
+    <div [innerHtml]="entity && entity.resources ? entity.resources : ''" | safeHtml></div>
     <a mat-raised-button *ngIf="!userIsLoggedIn" [href]="'//www.usanpn.org/user/register?default_group_id='+entity.network_id">Register</a>
     <a mat-raised-button *ngIf="!userIsLoggedIn" href="/user/login">Login</a>
     <a mat-raised-button *ngIf="userIsLoggedIn" href="//mynpn.usanpn.org/npnapps/" target="_blank">My Observation Deck</a>
